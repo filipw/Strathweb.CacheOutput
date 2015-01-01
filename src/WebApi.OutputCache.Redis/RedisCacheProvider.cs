1c901a9a08fa9374b8f892983adc633cc25de7af
imperugo <imperugo@gmail.com>
2015-01-01 18:19:00 +0100

    Added Redis Cache Provider with Unit tests

---
+++ src/WebApi.OutputCache.Redis/RedisCacheProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.Redis;
+using WebApi.OutputCache.Core.Cache;
+
+namespace WebApi.OutputCache.Redis
+{
+	public class RedisCacheProvider : IApiOutputCache
+	{
+		private readonly IRedisClient _redisClient;
+
+		public RedisCacheProvider(string host)
+		{
+			_redisClient = new RedisClient(host);
+		}
+
+		public RedisCacheProvider(string host, int port, string password = null, long db = 0L)
+		{
+			_redisClient = new RedisClient(host, port, password, db);
+		}
+
+		public RedisCacheProvider(IRedisClient redisClient)
+		{
+			_redisClient = redisClient;
+		}
+
+		public void RemoveStartsWith(string key)
+		{
+			var keys = _redisClient.SearchKeys(string.Format("{0}*", key));
+			_redisClient.RemoveAll(keys);
+
+			//var items = _redisClient.GetAllItemsFromList(key);
+			//foreach (var item in items)
+			//{
+			//	Remove(item);
+			//}
+			//Remove(key);
+		}
+
+		public T Get<T>(string key) where T : class
+		{
+			return _redisClient.Get<T>(key);
+		}
+
+		public object Get(string key)
+		{
+			var result = _redisClient.Get<object>(key);
+			return result;
+		}
+
+		public void Remove(string key)
+		{
+			_redisClient.Remove(key);
+		}
+
+		public bool Contains(string key)
+		{
+			return _redisClient.ContainsKey(key);
+		}
+
+		public void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null)
+		{
+			var primaryAdded = _redisClient.Add(key, o, expiration.Subtract(DateTimeOffset.Now));
+
+			if (dependsOnKey != null && primaryAdded)
+			{
+				_redisClient.AddItemToList(dependsOnKey, key);
+			}
+		}
+
+		public IEnumerable<string> AllKeys
+		{
+			get { return _redisClient.GetAllKeys(); }
+		}
+	}
+}
---
+++ test/WebApi.OutputCache.Redis.Tests/RedisCacheProviderTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using ServiceStack.Redis;
+
+namespace WebApi.OutputCache.Redis.Tests
+{
+	[TestFixture]
+	public class RedisCacheProviderTest
+	{
+		private IRedisClient _redisClient;
+		private RedisCacheProvider _testClass;
+
+		[SetUp]
+		public void fixture_init()
+		{
+			_redisClient = new RedisClient("bmwremarketingqa.redis.cache.windows.net", 6379, "Ecdg3Rc+8bESAIqq4DmxGRBb7NjjRLLekaG7nuPMQ/8=");
+			_testClass = new RedisCacheProvider(_redisClient);
+		}
+
+		[TearDown]
+		public void fixture_dispose()
+		{
+			var keys = _redisClient.GetAllKeys();
+			_redisClient.RemoveAll(keys);
+
+			_redisClient.Dispose();
+			_testClass = null;
+		}
+
+		[Test]
+		public void remove_starts_with_method_should_remove_the_correct_keys()
+		{
+			_redisClient.Add("MyCacheKey1", "something", TimeSpan.FromMinutes(1));
+			_redisClient.Add("MyCacheKey2", "something", TimeSpan.FromMinutes(1));
+			_redisClient.Add("MyCacheKey3", "something", TimeSpan.FromMinutes(1));
+			_redisClient.Add("CacheKey1", "something", TimeSpan.FromMinutes(1));
+			_redisClient.Add("CacheKey2", "something", TimeSpan.FromMinutes(1));
+			_redisClient.Add("CacheKey3", "something", TimeSpan.FromMinutes(1));
+
+			_testClass.RemoveStartsWith("MyCache");
+
+			var keys = _redisClient.GetAllKeys();
+
+			Assert.IsFalse(keys.Contains("MyCacheKey1"));
+			Assert.IsFalse(keys.Contains("MyCacheKey2"));
+			Assert.IsFalse(keys.Contains("MyCacheKey3"));
+			Assert.IsTrue(keys.Count == 3);
+		}
+
+		[Test]
+		public void add_method_with_dependency_should_add_a_list_into_redis()
+		{
+			_testClass.Add("MyCacheKey1", "something", DateTimeOffset.Now.AddMinutes(2), "DependencyKey");
+			_testClass.Add("MyCacheKey2", "something", DateTimeOffset.Now.AddMinutes(2), "DependencyKey");
+
+			var keys = _redisClient.GetAllItemsFromList("DependencyKey");
+
+			Assert.IsTrue(keys.Count == 2);
+		}
+	}
+}
