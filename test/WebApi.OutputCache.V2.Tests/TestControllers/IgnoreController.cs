e2f6a7b8ad64316f11fb074b4f19e5538d7c7e0e
filipw <wojcieszyn@gmail.com>
2015-07-30 12:09:04 +0200

    added tests for ignoring cache

---
+++ test/WebApi.OutputCache.V2.Tests/TestControllers/IgnoreController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace WebApi.OutputCache.V2.Tests.TestControllers
+{
+    [CacheOutput(ClientTimeSpan = 100, ServerTimeSpan = 100)]
+    public class IgnoreController : ApiController
+    {
+        [HttpGet]
+        public string Cached()
+        {
+            return "test";
+        }
+
+        [HttpGet]
+        [IgnoreCacheOutput]
+        public string NotCached()
+        {
+            return "test";
+        }
+    }
+}
